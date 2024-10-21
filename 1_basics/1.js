5dfc8a6cf22c3f4400cc165a61712a398c9385b4
Mohit Singh <devbyte.mohit@gmail.com>
2024-10-21 18:42:31 +0000

    dataTypes

---
+++ 1_basics/1.js
@@ -0,0 +1,9 @@
+accountID = 14439487
+let accountEmail = "mohitdebian@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountID = 1334343
+accountEmail= "hcj@gmail.com"
+console.table([accountEmail, accountCity, accountID, accountPassword, accountState])
